c282d663d960b8b46fa4facd819685d4e921ae25
samuelselasi <samelsekasi@gmail.com>
2023-10-28 22:24:49 +0000

    Add method to test if utils.get_json returns the expected result

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,10 +3,12 @@
 from unittest import TestCase
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import get_json
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(TestCase):
-    """Class that defines attributes to test functions"""
+    """Class that defines attributes to test utils.access_nested_map func"""
 
     @parameterized.expand([({"a": 1}, ("a",), 1), ({"a": {
         "b": 2}}, ("a",), {"b": 2}), ({"a": {"b": 2}}, ("a", "b"), 2)])
@@ -22,3 +24,17 @@ class TestAccessNestedMap(TestCase):
         with self.assertRaises(KeyError) as err:
             access_nested_map(nested_map, path)
         self.assertEqual(err.exception.args[0], path[-1])
+
+
+class TestGetJson(TestCase):
+    """Class that defines attributes to test utils.get_json function"""
+
+    @parameterized.expand([("http://example.com", {
+        "payload": True}), ("http://holberton.io", {"payload": False})])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock):
+        """Method to test that utils.get_json returns the expected result"""
+
+        mock.return_value = test_payload
+        res = get_json(test_url)
+        self.assertEqual(res, test_payload)
