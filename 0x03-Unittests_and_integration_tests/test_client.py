a75eece9a2c713f949b2a301827d04f4db0686d0
samuelselasi <samelsekasi@gmail.com>
2023-10-30 19:45:10 +0000

    Atempt fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,16 +28,15 @@ class TestGithubOrgClient(TestCase):
     def test_public_repos_url(self):
         """Method to test GithubOrgClient._public_repos_url function"""
 
-        with patch.object(GithubOrgClient, 'org',
-                          new_callable=PropertyMock,
-                          return_value={"repos_url": "Test value"}
-                          ) as mock:
-            test_json = {"repos_url": "Test value"}
-            client = GithubOrgClient(test_json.get("repos_url"))
-            res = client._public_repos_url
-
-            self.assertEqual(res, mock.return_value.get("repos_url"))
-            mock.assert_called_once
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock,
+        ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos"}
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos")
 
     @patch("client.get_json", return_value=[{"name": "Test value"}])
     def test_public_repos(self, mock):
