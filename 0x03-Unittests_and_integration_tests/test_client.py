ef10ad532b77aaf216e09260362b6a54e0e1899d
samuelselasi <samelsekasi@gmail.com>
2023-10-30 19:15:10 +0000

    Modify wth type annotations

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,7 +73,7 @@ class TestIntegrationGithubOrgClient(TestCase):
     """Class that defines attributes to test client.GithubOrgClient class"""
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls) -> None:
         """Method to prepare test fixture"""
 
         route_payload = {
@@ -90,20 +90,20 @@ class TestIntegrationGithubOrgClient(TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
-    def test_public_repos(self):
+    def test_public_repos(self) -> None:
         """Method to test GithubOrgClient.public_repos function"""
 
         self.assertEqual(GithubOrgClient("google").public_repos(),
                          self.expected_repos)
 
-    def test_public_repos_with_license(self):
+    def test_public_repos_with_license(self) -> None:
         """Method to test the public_repos with the argument license"""
 
         self.assertEqual(GithubOrgClient(
             "google").public_repos(license="apache-2.0"), self.apache2_repos)
 
     @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(cls) -> None:
         """Method called after test method has been called"""
 
         cls.get_patcher.stop()
