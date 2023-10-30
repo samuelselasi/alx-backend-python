dfac4fd5360dfa26c30d86c20f6893a2d8c95e29
samuelselasi <samelsekasi@gmail.com>
2023-10-30 18:33:31 +0000

    Revert to first commit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,7 +8,6 @@ from unittest.mock import PropertyMock
 from fixtures import TEST_PAYLOAD
 from urllib.error import HTTPError
 from parameterized import parameterized_class
-from unittest.mock import Mock
 
 
 class TestGithubOrgClient(TestCase):
@@ -65,10 +64,8 @@ class TestGithubOrgClient(TestCase):
         self.assertEqual(ret, res)
 
 
-@parameterized_class([{"org_payload": TEST_PAYLOAD[0][0],
-                       "repos_payload": TEST_PAYLOAD[0][1],
-                       "expected_repos": TEST_PAYLOAD[0][2],
-                       "apache2_repos": TEST_PAYLOAD[0][3]}])
+@parameterized_class(("org_payload", "repos_payload", "expected_repos",
+                     "apache2_repos"), TEST_PAYLOAD)
 class TestIntegrationGithubOrgClient(TestCase):
     """Class that defines attributes to test client.GithubOrgClient class"""
 
@@ -76,28 +73,17 @@ class TestIntegrationGithubOrgClient(TestCase):
     def setUpClass(cls):
         """Method to prepare test fixture"""
 
-        route_payload = {
-            'https://api.github.com/orgs/google': cls.org_payload,
-            'https://api.github.com/orgs/google/repos': cls.repos_payload}
-
-        def get_payload(url):
-            """Function that handles urls"""
-
-            if url in route_payload:
-                return Mock(**{'json.return_value': route_payload[url]})
-            return HTTPError
-
-        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher = patch('requests.get', side_effect=HTTPError)
         cls.get_patcher.start()
-
-    def test_public_repos(self):
-        """Method to test GithubOrgClient.public_repos function"""
-
-        self.assertEqual(GithubOrgClient("google").public_repos(),
-                         self.expected_repos)
 
     @classmethod
     def tearDownClass(cls):
         """Method called after test method has been called"""
 
         cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Method to test GithubOrgClient.public_repos function"""
+
+        res = GithubOrgClient("Test value")
+        self.assertTrue(res)
